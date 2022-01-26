7de97147c5f2a0f4fc2f87928ff3f51336d17996
JulienAlleaume <julien.alleaume@etu.umontpellier.fr>
2022-01-26 09:53:28 +0100

    Mise a jour

---
+++ SAE15CodeStatistique.py
@@ -0,0 +1,32 @@
+from encodings import utf_8
+
+f1 = open("DonnéesParkings.txt","a")
+DonnéesParkings = f1.readline()
+f1.closes()
+
+print(DonnéesParkings)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
