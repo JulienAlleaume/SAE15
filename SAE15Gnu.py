1fc3861afd7db53f97df3c39acbc44f9bfcf1531
JulienAlleaume <96527482+JulienAlleaume@users.noreply.github.com>
2022-01-31 19:41:18 +0100

    Add files via upload

---
+++ SAE15Gnu.py
@@ -0,0 +1,24 @@
+#Chargement des données Voiture
+f1 = open("DATAVoiture.csv","r")
+print(f1.readline())[0 : 4]
+f1.close()
+
+
+
+
+
+
+
+
+
+
+
+
+#Chargement des données Velo
+f1 = open("DATAVelo.csv","r")
+print(f1.readline())
+f1.close()
+
+
+
+
